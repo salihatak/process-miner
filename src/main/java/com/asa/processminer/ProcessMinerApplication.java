48844976259aca5fcbca8ba0d29e5a2f1ac89662
salih <salih.atak@gmail.com>
2020-02-19 22:10:19 +0300

    Initial commit

---
+++ src/main/java/com/asa/processminer/ProcessMinerApplication.java
@@ -0,0 +1,13 @@
+package com.asa.processminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessMinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessMinerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asa/processminer/ProcessMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asa.processminer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessMinerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
